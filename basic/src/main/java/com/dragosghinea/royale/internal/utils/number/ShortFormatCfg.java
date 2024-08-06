2eb027ab54c583a41055e8b835c815a8cfe9749c
DragosGhinea <dragos.dev12@gmail.com>
2024-08-06 23:21:53 +0300

    Add constructors for ShortFormat config classes

--- basic/src/main/java/com/dragosghinea/royale/internal/utils/number/ShortFormatCfg.java
+++ basic/src/main/java/com/dragosghinea/royale/internal/utils/number/ShortFormatCfg.java
@@ -3,13 +3,17 @@ package com.dragosghinea.royale.internal.utils.number;
 import com.dragosghinea.yaml.ConfigValues;
 import com.dragosghinea.yaml.annotations.Comments;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Map;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShortFormatCfg extends ConfigValues {
 
     @Comments({
--- basic/src/main/java/com/dragosghinea/royale/internal/utils/number/ShortFormatPairCfg.java
+++ basic/src/main/java/com/dragosghinea/royale/internal/utils/number/ShortFormatPairCfg.java
@@ -2,11 +2,15 @@ package com.dragosghinea.royale.internal.utils.number;
 
 import com.dragosghinea.yaml.ConfigValues;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShortFormatPairCfg extends ConfigValues {
 
     @JsonProperty("number-of-zeros")
