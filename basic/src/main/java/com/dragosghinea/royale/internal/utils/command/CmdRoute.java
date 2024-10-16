a48bee1076778f57fff6ea7bc2474d9bf58343bc
DragosGhinea <dragos.dev12@gmail.com>
2024-10-16 12:39:21 +0300

    Adding a default for execute in CmdRoute

--- basic/src/main/java/com/dragosghinea/royale/internal/utils/command/CmdRoute.java
+++ basic/src/main/java/com/dragosghinea/royale/internal/utils/command/CmdRoute.java
@@ -14,7 +14,24 @@ public interface CmdRoute {
 
     boolean canExecute(CmdContext context, boolean silentCheck);
 
-    boolean execute(CmdContext context);
+    default boolean execute(CmdContext context) {
+        StringBuilder builder = new StringBuilder("/");
+        builder.append(context.getCommand().getName());
+        context.getPassedThrough().forEach(route -> builder.append(" ").append(route.getName()));
+        builder.append(" ").append(getName());
+        String prefix = builder.toString();
+
+        subRoutes().values().stream()
+                .filter(cmdRoute -> cmdRoute.canExecute(context, true))
+                .map(routeCmd -> routeCmd.getDescription().isEmpty() ?
+                        String.format("%s%s %s", context.getCommand().getColor(), prefix, routeCmd.getName())
+                        :
+                        String.format("%s%s %s §7- §f%s", context.getCommand().getColor(), prefix, routeCmd.getName(), routeCmd.getDescription())
+                )
+                .forEach(s -> context.getSender().sendMessage(s));
+
+        return true;
+    }
 
     default List<String> tabComplete(CmdContext context) {
         String currentInput = context.getArgsLeftToProcess().isEmpty() ? "" : context.getArgsLeftToProcess().lastElement();
